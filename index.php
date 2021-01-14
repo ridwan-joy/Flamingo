5e003d71ea12ea845f8f9013dd7a9b5b5caa9819
Mahmud Ridwan Joy <mahmudridwan.cs@gmail.com>
2021-01-14 20:54:28 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,216 @@
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <title>Project</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Dosis:400,700' rel='stylesheet' type='text/css'>
+
+        <!-- Bootsrap -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+        <!-- Font awesome -->
+        <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+        <!-- Owl carousel -->
+        <link rel="stylesheet" href="assets/css/owl.carousel.css">
+
+        <!-- Template main Css -->
+        <link rel="stylesheet" href="assets/css/style.css">
+        
+        <!-- Modernizr -->
+        <script src="assets/js/modernizr-2.6.2.min.js"></script>
+
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    </head>
+
+
+<style type="text/css">
+
+.navbar-static-top {
+  margin: 0;
+  border: 0;
+  color: #fff;
+}
+
+.navbar-top {
+  background: #115c9b;
+  font-size: 12px;
+  padding: 3px 0;
+}
+.navbar-top .list-inline {
+  margin-bottom: 0;
+}
+.navbar-top .header-contact li {
+  margin-right: 15px;
+}
+.navbar-top .header-contact li .fa {
+  margin-right: 5px;
+}
+
+.navbar-main {
+  margin-bottom: 0;
+  color: #fff;
+  background: #1f76bd;
+}
+.navbar-main .navbar-brand {
+  border-bottom: none !important;
+}
+.navbar-main a:hover,
+.navbar-main a:active,
+.navbar-main a:focus,
+.navbar-main a.is-active {
+  background: none !important;
+  color: inherit;
+  border-bottom: 2px solid rgb(195, 255, 255);
+}
+.navbar-main .submenu {
+  position: absolute;
+  list-style: none;
+  background: #1f76bd;
+  padding: 0;
+  width: 150px;
+  left: -35px;
+  visibility: hidden;
+  top: 85px;
+  -moz-transition: all, 0.05s;
+  -o-transition: all, 0.05s;
+  -webkit-transition: all, 0.05s;
+  transition: all, 0.05s;
+}
+.navbar-main .submenu li {
+  border-top: 1px solid #185b91;
+}
+.navbar-main .submenu li:hover {
+  background: #185b91;
+  -moz-transition: all, 0.3s;
+  -o-transition: all, 0.3s;
+  -webkit-transition: all, 0.3s;
+  transition: all, 0.3s;
+}
+.navbar-main .submenu li a {
+  width: 100%;
+  border-bottom: none;
+  text-decoration: none;
+  padding: 5px 25px;
+  display: block;
+  font-size: 16px;
+}
+.navbar-main li a {
+  font-family: "Dosis", sans-serif;
+  font-size: 18px;
+  padding: 20px 15px;
+  border-bottom: 2px solid transparent;
+}
+.navbar-main li:hover .submenu {
+  top: 62px;
+  visibility: visible;
+  -moz-transition: all, 0.25s;
+  -o-transition: all, 0.25s;
+  -webkit-transition: all, 0.25s;
+  transition: all, 0.25s;
+}
+.navbar-main .icon-bar {
+  background: black;
+}
+
+
+
+
+
+.nav-tabs > li > a {
+  border-radius: 0;
+}
+
+.nav-tabs > li > a:hover {
+  border-color: black;
+}
+
+.nav > li > a:hover, .nav > li > a:focus {
+  background: #115c9b;
+  color: black;
+}
+
+
+
+.btn {
+  flex: 1 1 auto;
+  margin: 10px;
+  padding: 15px;
+  text-align: center;
+  text-transform: uppercase;
+  transition: 0.5s;
+  background-size: 200% auto;
+  color: white;
+ /* text-shadow: 0px 0px 10px rgba(0,0,0,0.2);*/
+  box-shadow: 0 0 20px #eee;
+  border-radius: 10px;
+  width: 200px;
+  border-radius: 30px
+ }
+
+.btn-4 {
+  background-image: linear-gradient(to right, #84fab0 0%, #8fd3f4 51%, #84fab0 100%);
+}
+.btn:hover {
+  background-position: right center;
+}
+
+
+</style>
+
+
+    <body>
+
+
+   <?php  include("navbar.php"); ?> <!-- /. main-header -->
+
+
+
+<div class="container">
+<div class="row"> 
+	<div class="col-sm-6">
+		 <small><b>Smartest digital platform</b></small>
+ <h1 style="font-family:Monospace; font-size: 50px;"><b>Go beyond your imagination with Flamingo</b></h1> 
+
+<small>
+  Lorem Ipsum is simply dummy text of the<br> printing and typesetting industry.
+</small>
+<br>
+<a class="btn btn-4">Sign up for free</a> 
+</div>
+<div class="col-sm-6">
+  <img src="assets/images/landing.png" style="width: 100%; height: 100%">
+</div>
+
+
+</div> 
+</div>
+
+
+
+
+
+    <!--  Scripts
+    ================================================== -->
+
+    <!-- jQuery -->
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="assets/js/jquery-1.11.1.min.js"><\/script>')</script>
+
+    <!-- Bootsrap javascript file -->
+    <script src="assets/js/bootstrap.min.js"></script>
+    
+    <!-- owl carouseljavascript file -->
+    <script src="assets/js/owl.carousel.min.js"></script>
+
+    <!-- Template main javascript -->
+    <script src="assets/js/main.js"></script>
+
+   
+
+    </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,50 @@
+
+ <header class="main-header" >
+        
+    
+        <nav class="navbar navbar-static-top"  >
+
+         
+
+            <div class="navbar-main" style="background-color: white " >
+              
+              <div class="container">
+                
+                <div class="navbar-header" >
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+
+                  </button>
+                  
+                   <a class="navbar-brand" href="index.php"><img src="assets/images/logo.png" style="width: 200px; height: 40px"></a>
+                  
+                </div>
+
+                <div id="navbar" class="navbar-collapse collapse pull-right">
+
+                  <ul class="nav navbar-nav" style="color: black">
+
+                 
+                   
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Admin-login</a></li>
+                    <li><a href="#">login</a></li>
+                  
+
+                  </ul>
+
+                </div> <!-- /#navbar -->
+
+              </div> <!-- /.container -->
+              
+            </div> <!-- /.navbar-main -->
+
+
+        </nav> 
+
+    </header>
